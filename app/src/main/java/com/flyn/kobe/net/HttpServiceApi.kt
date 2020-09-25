d9379bd7c262fc773fe4a82e4ea34497658c7496
yuyingjie <yuyingjie@kedacom.com>
2020-09-25 16:55:30 +0800

    1

--- app/src/main/java/com/flyn/kobe/net/HttpServiceApi.kt
+++ app/src/main/java/com/flyn/kobe/net/HttpServiceApi.kt
@@ -10,10 +10,10 @@ interface HttpServiceApi {
     @GET("v2/banners")
     suspend fun getBannerData(): BaseData<List<BannerData>>
 
-    @GET("v2/categories/Article")
+    @GET("v2/categories/GanHuo")
     suspend fun getCategoriesData(): BaseData<List<CategoryData>>
 
-    @GET("v2/data/category/Article/type/{category}/page/{pageNum}/count/{pageSize}")
+    @GET("v2/data/category/GanHuo/type/{category}/page/{pageNum}/count/{pageSize}")
     suspend fun getArticleData(@Path("category") category: String, @Path("pageNum") pageNum: Int, @Path("pageSize") pageSize: Int): BaseData<List<ArticleData>>
 
     @GET("http://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1")
